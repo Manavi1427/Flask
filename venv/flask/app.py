2a7fbf27c75c74ce87a5d56f235d0762bedf6919
Manavi1427 <manavisarkar136@gmail.com>
2025-04-29 15:28:31 +0530

    initial commit 2

--- venv/flask/app.py
+++ venv/flask/app.py
@@ -1,3 +1,14 @@
 from flask import Flask
 
-app=Flask()    #creating an instance of Flask
+app=Flask(__name__)    #creating an instance of Flask for WSGI
+
+@app.route('/')
+def welcome():
+    return "hello world, this is"
+
+@app.route('/index')
+def index():
+    return "index page"
+
+if __name__=="__main__":
+    app.run(debug=True)
